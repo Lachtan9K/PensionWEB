e8e2646ddb564fac8bb507d9923291ad0513ba88
Lachtan9K <duben.cz@seznam.cz>
2025-01-21 14:04:16 +0100

    uprava webu

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ export default async function (eleventyConfig) {
     //configure eleventy
     eleventyConfig.setInputDirectory("src");
     eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.setIncludesDirectory("./templates");
     eleventyConfig.setTemplateFormats("html,njk");
   }
